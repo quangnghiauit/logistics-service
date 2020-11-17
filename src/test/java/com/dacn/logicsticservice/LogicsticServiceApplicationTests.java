bdfef42d77c8d99fe4f2ccee2eed531c19adb0d2
nghianq3 <nghianq3@vng.com.vn>
2020-11-17 14:29:39 +0700

    update config

---
+++ src/test/java/com/dacn/logicsticservice/LogicsticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dacn.logicsticservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicsticServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
