180510f417250a7395af4d7d9050484f5432a1c5
nghianq3 <nghianq3@vng.com.vn>
2021-02-20 14:12:36 +0700

    order by Id desc

--- src/main/java/com/dacn/logicsticservice/repository/OrderRepository.java
+++ src/main/java/com/dacn/logicsticservice/repository/OrderRepository.java
@@ -15,9 +15,9 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
     @Query(value = "select * from `orders` where ID = ?1", nativeQuery = true)
     Order getAllById(Integer id);
 
-    @Query(value = "select * from `orders` where CusID = ?1", nativeQuery = true)
+    @Query(value = "select * from `orders` where CusID = ?1 order by ID desc", nativeQuery = true)
     List<Order> getAllByCusId(Integer cusId);
 
-    @Query(value = "select * from `orders` where RulID = ?1", nativeQuery = true)
+    @Query(value = "select * from `orders` where RulID = ?1 order by ID desc", nativeQuery = true)
     List<Order> getAllByRulID(Integer rulId);
 }
